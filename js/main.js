c2ac09799ad1cc5c7e644d5cd321b708c7705174
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-03-07 17:38:44 +0100

    a

--- js/main.js
+++ js/main.js
@@ -10,8 +10,30 @@ function initCannon(){
 
     var groundShape = new CANNON.Plane();
     groundBody = new CANNON.Body({ mass: 0, shape: groundShape });
-    world.add(groundBody);
+    // world.add(groundBody);
 
+    // Create a matrix of height values
+            var matrix = [];
+            var sizeX = 15,
+                sizeY = 15;
+            for (var i = 0; i < sizeX; i++) {
+                matrix.push([]);
+                for (var j = 0; j < sizeY; j++) {
+                    var height = Math.cos(i/sizeX * Math.PI * 2)*Math.cos(j/sizeY * Math.PI * 2) + 2;
+                    if(i===0 || i === sizeX-1 || j===0 || j === sizeY-1)
+                        height = 3;
+                    matrix[i].push(height);
+                }
+            }
+
+            // Create the heightfield
+            var hfShape = new CANNON.Heightfield(matrix, {
+                elementSize: 1
+            });
+            var hfBody = new CANNON.Body({ mass: 0 });
+            hfBody.addShape(hfShape);
+            hfBody.position.set(-sizeX * hfShape.elementSize / 2, -20, -10);
+            world.add(hfBody);
 }
 
   
